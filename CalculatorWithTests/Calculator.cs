f85c22046b43a4a4519e6238c6883052503e26b4
Kris <krisdixon@yahoo.com>
2019-07-09 17:18:22 -0500

    created 5 class methods

--- CalculatorWithTests/Calculator.cs
+++ CalculatorWithTests/Calculator.cs
@@ -1,8 +1,50 @@
-﻿
+﻿using System;
+using CalculatorWithTests;
+using System.Linq;
+
+
 namespace CalculatorWithTests
 {
-    class Calculator
+    public class Calculator
     {
-        
+        public int Add(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Subtract(int sub1, int sub2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Multiply(int multiply1, int multiply2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Divide(int dividend, int divisor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Factorial(int factNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+using System;
+using Xunit;
+using CalculatorWithTests;
+using System.Linq;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(12, 2, 14)]
+        [InlineData(0, 2, 2)]
+        [InlineData(10000, 2, 10002)]
+        [InlineData(5, 7, 12)]
+        [InlineData(-1, 0, -1)]
+        [InlineData(20, 20, 40)]
+        [InlineData(50, 50, 100)]
+        [InlineData(1, 1, 2)]
+        public void Add(int addend1, int addend2, int sum)
+        {
+            // Arrange
+            UnitTest1 unitTest = new UnitTest1();
+
+            // Act
+            int actual = unitTest.Add(addend1, addend2);
+
+            // Assert
+            Assert.Equal(sum, actual);
+
+
+
+
+        }
+    }
+}
